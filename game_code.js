7bdb88854570fe6b454c4aff015a73c2a759c339
boomcat <92338117+bhomcatt@users.noreply.github.com>
2022-01-06 15:29:40 -0600

    Add files via upload

---
+++ game_code.js
@@ -0,0 +1,8 @@
+document.getElementById("score1").innerHTML="puntuacion : ";
+document.getElementById("score2").innerHTML="puntuacion : ";
+function question(){
+num1 = document.getElementById("num1").value;
+num2 = document.getElementById("num2").value;
+result = num1*num2;
+console.log(result);
+}
